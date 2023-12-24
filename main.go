e2859a8e741ff25f02ba40764f1e32dbb5f7abb9
wipdev-tech <wipdev.tech@gmail.com>
2023-12-24 19:19:55 +0200

    REPL with help and exit commands

--- main.go
+++ main.go
@@ -1,7 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+type cliCmd struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+var cmds = map[string]cliCmd{
+	"help": {
+		name:        "help",
+		description: "Displays a help message",
+		callback:    cmdHelp,
+	},
+	"exit": {
+		name:        "exit",
+		description: "Exit the Pokedex",
+		callback:    cmdExit,
+	},
+}
 
 func main() {
-    fmt.Println("Hello world!")
+	fmt.Print("Pokedex > ")
+	s := bufio.NewScanner(os.Stdin)
+	for s.Scan() {
+		userCmd := s.Text()
+		cmds[userCmd].callback()
+		fmt.Print("Pokedex > ")
+	}
+}
+
+func cmdHelp() error {
+	fmt.Println(`
+Welcome to the Pokedex!
+
+Usage:
+help  Displays a help message
+exit  Exit the Pokedex`)
+    fmt.Println()
+	return nil
+}
+
+func cmdExit() error {
+	fmt.Println("Bye!")
+	os.Exit(0)
+	return nil
 }
